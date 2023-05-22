f89863c24ec8d815758ea60e87f315e92c303f5d
aralukene <59538075+aralukene@users.noreply.github.com>
2023-05-22 18:54:01 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,14 +3,14 @@
 import requests
 import sys
 
-
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    name = requests.get(f"{url}users/{sys.argv[1]}").json()
-    tasks = requests.get(f"{url}users/{sys.argv[1]}/todos").json()
-    c_tasks = [task for task in tasks if task.get("completed")]
-    titles = [task.get("title") for task in tasks if task.get("completed")]
-    
-    print(f"Employee {name.get('name')} is done with tasks({len(c_tasks)}/{len(tasks)}):")
+    name = requests.get("{}users/{}".format(url, sys.argv[1])).json()
+    tasks = requests.get("{}users/{}/todos".format(url, sys.argv[1])).json()
+    c_tasks = [task for task in tasks if task.get("completed") is True]
+    titles = [task.get("title") for task in tasks
+              if task.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name.get("name"), len(c_tasks), len(tasks)))
     for title in titles:
-        print(f"\t{title}")
+        print("\t {}".format(title))
